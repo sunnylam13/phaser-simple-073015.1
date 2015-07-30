2f0201030792c889992b7df1bcb9137267c58747
Sunny Lam <sunny.lam@gmail.com>
2015-07-30 11:33:18 -0400

    changed the gulp file for multiple pages in case I decide to create variants in the same project

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('jade', function() {
 
 	// using src = ./*.jade causes index.layout.jade to also compile which we don't want... unless we have multiple main directory files... in which case we do use ./*.jade
 	// otherwise use src = ./index.jade if there aren't other jade files in ./ (i.e. contact.jade, about.jade, etc.)
-	return gulp.src('./index.jade')
+	return gulp.src('./*.jade')
 		.pipe(plumber())
 		.pipe(jade({
 			locals: jade_locals,
